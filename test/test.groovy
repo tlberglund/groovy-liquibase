0cdc6738fd3f1d5577244e8c8bb089e5255a69ed
tlberglund <tlberglund@gmail.com>
2011-03-19 11:20:20 -0600

    Added some changelogs being used for ad-hoc testing. Should formalize and automate these in a future commit.

---
+++ test/test.groovy
@@ -0,0 +1,29 @@
+databaseChangeLog() {
+
+  include(path: 'changelogs')
+
+
+  changeSet(id:'monkey', author: 'tlberglund') {
+    new File('test.sql').withPrintWriter { pw ->
+      pw.println "CREATE TABLE monkey (id int, mood varchar(50))"
+    }
+
+    sqlFile(path: 'test.sql')
+  }
+
+  changeSet(author: 'tlberglund', id: 'test-load-data') {
+    new File('data.csv').withPrintWriter { pw ->
+      pw.println "id,mood"
+      pw.println "1,angry"
+      pw.println "2,happy"
+      pw.println "3,ambivalent"
+    }
+
+    loadData(tableName: 'monkey', file: 'data.csv') {
+      column(name: 'id', type: "NUMERIC")
+      column(name: 'mood', type: "STRING")
+    }
+
+  }
+
+}
